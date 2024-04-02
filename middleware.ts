d78aae8302065132b9727b004965f2f84bec69b6
greghim53lf <greghim53lf+github@gmail.com>
2024-04-02 16:56:12 +0100

    third commit

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,11 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/", "/events/:id", "/api/webhook/clerk", "/api/uploadthing"],
   ignoredRoutes: ["/api/webhook/clerk", "/api/uploadthing"],
+  afterAuth: (auth, req, _) => {
+    console.log("auth", auth);
+    console.log("req", req);
+    console.log("other", _);
+  },
 });
 
 export const config = {
